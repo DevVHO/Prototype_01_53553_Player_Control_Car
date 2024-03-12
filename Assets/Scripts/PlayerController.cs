5909ec87298473c34d095f8f7cf7f4fb2e843b9e
DevVHO <a53553@alunos.ipb.pt>
2024-03-12 10:30:20 +0000

    stage 1.2
    pass 01
    Someone add a script in my project

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
     //Private Variable
@@ -9,6 +10,7 @@ public class PlayerController : MonoBehaviour
     private float turnSpeed = 25.0f;
     private float horizontalInput;
     private float forwardInput;
+
 
     // Start is called before the first frame update
     void Start()
