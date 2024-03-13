13508e441761d9d954526e859ae2fac6b82f8750
DevVHO <a53553@alunos.ipb.pt>
2024-03-13 19:19:31 +0000

    stage_1.4
    READY

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,16 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class PlayerController : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
-    //Private Variable
-    private float speed = 10.0f;
-    private float turnSpeed = 25.0f;
-    private float horizontalInput;
-    private float forwardInput;
-
-
+    public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,17 +14,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //This is where we get Player Input
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
-
-
-        // We move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        //We turn the vehicle
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
-
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
-    
