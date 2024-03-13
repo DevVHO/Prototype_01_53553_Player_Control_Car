95f5b31671b76c93d12c888b096ecee6d4e5762e
DevVHO <a53553@alunos.ipb.pt>
2024-03-13 18:25:39 +0000

    stage_1.3
    The stage 1.3 i have been put script in my car e making he to go for front. Put Colission in somebody assets(I am training to write in english).

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,6 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
