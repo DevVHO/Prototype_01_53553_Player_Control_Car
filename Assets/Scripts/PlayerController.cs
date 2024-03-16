3137f3d0b18ea04f6590c4e60fabb2d0cdfca2da
DevVHO <a53553@alunos.ipb.pt>
2024-03-16 18:09:22 +0000

    Stage 1.4 2
    In this stage i created a variable called "horizontalInput", With this Variable i modified anything properties of Input manager

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public float speed = 5.0f;
-    public float turnspeed;
+    public float turnSpeed;
+    public float horizontalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +16,9 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnspeed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
